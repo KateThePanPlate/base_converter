80b064dec1865a251633ccfcde04abdbdc5ea1fd
Kate <newoowend@gmail.com>
2024-11-23 13:53:57 -0500

    changed font

--- BaseGUI.py
+++ BaseGUI.py
@@ -41,40 +41,40 @@ def doCopy():
 root = tk.Tk()
 root.title("Base Converter Version "+str(bc.version)+" | GUI V. "+str(GUIVersion))
 
-label = tk.Label(root, text="Input's Base:", font=("Montserrat",16))
+label = tk.Label(root, text="Input's Base:", font=("Times New Roman",16))
 label.grid(row=0,column=0,padx=10,pady=10)
 
 #entry for the input base
-ent_b1 = tk.Entry(root, font=("Montserrat",14),width=8)
+ent_b1 = tk.Entry(root, font=("Times New Roman",14),width=8)
 ent_b1.grid(row=0,column=1,padx=10,pady=10)
 
 #entry for the number for conversion
-label = tk.Label(root, text="Input Number:", font=("Montserrat",16))
+label = tk.Label(root, text="Input Number:", font=("Times New Roman",16))
 label.grid(row=0,column=2,padx=10,pady=10)
 
-ent_i1 = tk.Entry(root, font=("Montserrat",14),width=16)
+ent_i1 = tk.Entry(root, font=("Times New Roman",14),width=16)
 ent_i1.grid(row=0,column=3,padx=10,pady=10)
 
-label = tk.Label(root, text="Output's Base:", font=("Montserrat",16))
+label = tk.Label(root, text="Output's Base:", font=("Times New Roman",16))
 label.grid(row=2,column=0,padx=10,pady=10)
 
 #entry for the output base
-ent_b2 = tk.Entry(root, font=("Montserrat",14),width=8)
+ent_b2 = tk.Entry(root, font=("Times New Roman",14),width=8)
 ent_b2.grid(row=2,column=1,padx=10,pady=10)
 
-label = tk.Label(root, text="Output:", font=("Montserrat",16))
+label = tk.Label(root, text="Output:", font=("Times New Roman",16))
 label.grid(row=2,column=2,padx=10,pady=10)
 
-answer = tk.Label(root, text=str(val), font=("Montserrat",16))
+answer = tk.Label(root, text=str(val), font=("Times New Roman",16))
 answer.grid(row=2,column=3,padx=10,pady=10)
 
-copy_button = tk.Button(root, text="copy", font=("Montserrat", 12), command=doCopy)
+copy_button = tk.Button(root, text="copy", font=("Times New Roman", 12), command=doCopy)
 copy_button.grid(row=3,column=3,padx=10,pady=10)
 
-swap_button = tk.Button(root, text="swap", font=("Montserrat", 12), command=swap_text)
+swap_button = tk.Button(root, text="swap", font=("Times New Roman", 12), command=swap_text)
 swap_button.grid(row=3,column=1,padx=10,pady=10)
 
-clear_button = tk.Button(root, text="clear", font=("Montserrat", 12), command=clear_text)
+clear_button = tk.Button(root, text="clear", font=("Times New Roman", 12), command=clear_text)
 clear_button.grid(row=3,column=0,padx=10,pady=10,sticky="W")
 
 root.after(0,conv)
